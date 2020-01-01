a8b737489400fcf77be82228b57eaf7e3d7aed2f
ba66e77 <barrett@datasmith.net>
2020-01-01 11:23:59 -0500

    Import data and output column names and first 5 rows.

--- 20200101-StateLegislatorIdealogies/analysis.py
+++ 20200101-StateLegislatorIdealogies/analysis.py
@@ -1,3 +1,6 @@
 import pandas as pd
 
-data = pd.io.stata.read_stata('my_stata_file.dta')
+data = pd.io.stata.read_stata('data/shor+mccarty+1993-2016+individual+legislator+data+May+2018+release+(Updated+July+2018).dta')
+
+print(data.columns)
+print(data.head())
